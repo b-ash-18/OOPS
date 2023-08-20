a0a48a06fd01cdf1ed923c980cd3d29f03d7b976
Ashish B <115473995+b-ash-18@users.noreply.github.com>
2023-08-20 22:25:10 +0530

    changed the menu

--- oopsLab14_a.cpp
+++ oopsLab14_a.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int main(){
@@ -7,7 +8,7 @@ int main(){
     int menu,e,index;
     while(true){
         cout << "Menu\n1.size\n2.empty\n3.insert\n4.remove at index\n" <<
-        "5.index of elemen\nt6.exit" << endl;
+        "5.index of element\n6.display\n7.exit" << endl;
         cin >> menu;
         switch(menu){
             case 1:
@@ -29,14 +30,31 @@ int main(){
             case 4:
                 cout << "Enter the index to remove :";
                 cin >> index;
-                v;
+                v.erase(v.begin()+index);
                 break;
             case 5:
+            {
                 cout << "Enter the elem to find its index :";
                 cin >> e;
-
-                cout << "Element found at index :" << endl;
+                auto it = find(v.begin(), v.end(), e);
+                if (it != v.end()){
+                    int index = it - v.begin();
+                    cout << "Element found at index :" << index << endl;
+                    break;
+                }
+                else{
+                    cout << "Element not found" << endl;
+                    break;
+                }
+            }
+            case 6:{
+                cout << "VECTOR: ";
+                for(int i = 0; i < v.size(); i++){
+                    cout << v[i] << " ";
+                }
+                cout << endl;
                 break;
+            }
             default:
                 cout << "Program terminating" << endl;
                 return 0;
